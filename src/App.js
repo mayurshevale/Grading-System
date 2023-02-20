3fef22c0cec551678f27aa0e89a94b02d34e63a5
Mayur Shevale <mayurshevale13@gmail.com>
2023-02-20 12:26:59 +0530

    Project added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import FileUpload from './components/Step_1';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Grading Helper
+      </h1>
+      <FileUpload/>
+
+      
+    </div>
+  );
+}
+
+export default App;
